ca4fa25f6ea9784b981da82e61655e852d57fcdd
Joshua Villing <joshua.villing@mimacom.com>
2021-03-13 11:37:20 +0100

    Initial commit

---
+++ src/test/java/ch/fhnw/ip5/praxiscloudservice/PraxisCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.ip5.praxiscloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxisCloudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
