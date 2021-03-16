9e94ca230b6c25f956784c4b0b44bf73572bdcb1
Joshua Villing <joshua.villing@mimacom.com>
2021-03-16 11:04:35 +0100

    Setup dummy notification service

---
+++ praxis-intercom-cloud-web/src/main/java/ch/fhnw/ip5/praxiscloudservice/cloud/notification/NotificationController.java
@@ -0,0 +1,24 @@
+package ch.fhnw.ip5.praxiscloudservice.cloud.notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("/send")
+    public void sendNotification(@RequestBody String message) {
+        notificationService.send(message);
+    }
+}
---
+++ praxis-intercom-cloud-web/src/main/java/ch/fhnw/ip5/praxiscloudservice/cloud/notification/NotificationService.java
@@ -0,0 +1,11 @@
+package ch.fhnw.ip5.praxiscloudservice.cloud.notification;
+
+/**
+ *
+ * @author J. Villing
+ */
+public interface NotificationService {
+
+    void send(String message);
+
+}
