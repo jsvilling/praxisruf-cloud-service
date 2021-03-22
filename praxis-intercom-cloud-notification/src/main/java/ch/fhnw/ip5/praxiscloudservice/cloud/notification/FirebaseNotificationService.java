98b1ca69ad8a0ea4e50a57ea4cf6f2ed144dea37
Joshua Villing <joshua.villing@mimacom.com>
2021-03-22 17:45:33 +0100

    Add notification to firebase message

--- praxis-intercom-cloud-notification/src/main/java/ch/fhnw/ip5/praxiscloudservice/cloud/notification/FirebaseNotificationService.java
+++ praxis-intercom-cloud-notification/src/main/java/ch/fhnw/ip5/praxiscloudservice/cloud/notification/FirebaseNotificationService.java
@@ -3,6 +3,7 @@ package ch.fhnw.ip5.praxiscloudservice.cloud.notification;
 import ch.fhnw.ip5.praxiscloudservice.cloud.web.NotificationService;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,19 +13,26 @@ public class FirebaseNotificationService implements NotificationService {
 
     // TODO: Get actual client token or topic
     private static final String TOPIC = "test";
-    private static final String KEY = "Some Key";
+    private static final String KEY = "Test Key";
 
     private final Logger log = LoggerFactory.getLogger(FirebaseNotificationService.class);
 
     @Override
     public void send(String message) throws Exception {
+
+        final Notification notification = Notification.builder()
+                .setTitle("This is a test title")
+                .setBody("This is a reasonableBody")
+                .build();
+
         final Message firebaseMessage = Message.builder()
                 .putData(KEY, message)
                 .setTopic(TOPIC)
+                .setNotification(notification)
                 .build();
 
-        FirebaseMessaging.getInstance().send(firebaseMessage);
+        final String response = FirebaseMessaging.getInstance().send(firebaseMessage);
 
-        log.info("Success: Message was sent.");
+        log.info("Success: Message {} was sent.", response);
     }
 }
