70e6724929226dc9c38e61911b1b6b0dc48b465d
Joshua Villing <joshua.villing@mimacom.com>
2021-05-30 07:38:02 +0200

    Use @Repository

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/persistence/ClientConfigurationRepository.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/persistence/ClientConfigurationRepository.java
@@ -2,8 +2,10 @@ package ch.fhnw.ip5.praxiscloudservice.persistence;
 
 import ch.fhnw.ip5.praxiscloudservice.domain.ClientConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ClientConfigurationRepository extends JpaRepository<ClientConfiguration, UUID> {
 }
--- src/main/java/ch/fhnw/ip5/praxiscloudservice/persistence/ClientRepository.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/persistence/ClientRepository.java
@@ -2,10 +2,12 @@ package ch.fhnw.ip5.praxiscloudservice.persistence;
 
 import ch.fhnw.ip5.praxiscloudservice.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 import java.util.UUID;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
 
     Set<Client> findAllByUserId(UUID userId);
