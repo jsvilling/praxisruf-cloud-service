0e14753cf46505e549bb4d847799e81e11ab0ff2
Joshua Villing <joshua.villing@mimacom.com>
2021-05-30 07:29:23 +0200

    Use @Repository

--- praxis-intercom-cloud-configuration/src/main/java/ch/fhnw/ip5/praxiscloudservice/RegistrationRepository.java
+++ praxis-intercom-cloud-configuration/src/main/java/ch/fhnw/ip5/praxiscloudservice/RegistrationRepository.java
@@ -1,9 +1,11 @@
 package ch.fhnw.ip5.praxiscloudservice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RegistrationRepository extends JpaRepository<Registration, String> {
 
     List<Registration> findAll();
