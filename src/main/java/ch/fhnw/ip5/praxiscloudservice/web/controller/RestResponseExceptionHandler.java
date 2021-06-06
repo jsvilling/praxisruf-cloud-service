26aa693e6c93c3fbb890ca2d7c3bf13e8c6b523b
Joshua Villing <joshua.villing@mimacom.com>
2021-06-06 10:15:16 +0200

    Add more error code to http status mapping

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/web/controller/RestResponseExceptionHandler.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/web/controller/RestResponseExceptionHandler.java
@@ -14,14 +14,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.EnumMap;
 import java.util.Map;
 
+import static ch.fhnw.ip5.praxiscloudservice.api.exception.ErrorCode.*;
+import static org.springframework.http.HttpStatus.*;
+
 @ControllerAdvice
 public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Map<ErrorCode, HttpStatus> ERROR_CODE_HTTP_STATUS_MAP = new EnumMap<ErrorCode, HttpStatus>(ErrorCode.class);
 
     static {
-        ERROR_CODE_HTTP_STATUS_MAP.put(ErrorCode.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
-        ERROR_CODE_HTTP_STATUS_MAP.put(ErrorCode.CLIENT_NOT_FOUND, HttpStatus.NOT_FOUND);
+        ERROR_CODE_HTTP_STATUS_MAP.put(USER_NOT_FOUND, NOT_FOUND);
+        ERROR_CODE_HTTP_STATUS_MAP.put(CLIENT_NOT_FOUND, NOT_FOUND);
+        ERROR_CODE_HTTP_STATUS_MAP.put(FCM_ERROR, INTERNAL_SERVER_ERROR);
+        ERROR_CODE_HTTP_STATUS_MAP.put(INVALID_NOTIFICATION_TYPE, BAD_REQUEST);
     }
 
     @ExceptionHandler(PraxisIntercomException.class)
@@ -31,12 +36,12 @@ public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler
     }
 
     private HttpStatus determineResponseStatus(ErrorCode errorCode) {
-        return ERROR_CODE_HTTP_STATUS_MAP.getOrDefault(errorCode, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ERROR_CODE_HTTP_STATUS_MAP.getOrDefault(errorCode, INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(value = INTERNAL_SERVER_ERROR)
     protected ResponseEntity<Object> handleGeneralException(Exception e, WebRequest w) {
-        return handleExceptionInternal(e, "An unexpected error occured", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, w);
+        return handleExceptionInternal(e, "An unexpected error occurred", new HttpHeaders(), INTERNAL_SERVER_ERROR, w);
     }
 }
