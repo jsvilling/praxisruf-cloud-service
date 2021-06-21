d11966d50c1ed8d4f26f88f40998e4c286c748ac
Joshua Villing <joshua.villing@mimacom.com>
2021-06-21 10:18:03 +0200

    Add register endpoint

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/domain/PraxisIntercomUser.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/domain/PraxisIntercomUser.java
@@ -1,9 +1,6 @@
 package ch.fhnw.ip5.praxiscloudservice.domain;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,6 +14,7 @@ import java.util.UUID;
  *
  */
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // for JPA
 @Getter
 @Builder
