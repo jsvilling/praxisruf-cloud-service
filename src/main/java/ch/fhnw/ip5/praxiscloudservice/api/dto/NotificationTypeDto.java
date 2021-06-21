e5f249256191f17d4a39e170b6123fd49c881683
Joshua Villing <joshua.villing@mimacom.com>
2021-06-21 16:20:51 +0200

    Add id to notification type dto

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/api/dto/NotificationTypeDto.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/api/dto/NotificationTypeDto.java
@@ -3,9 +3,12 @@ package ch.fhnw.ip5.praxiscloudservice.api.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @Builder
 public class NotificationTypeDto {
+    private UUID notificationTypeId;
     private String displayText;
     private String title;
     private String body;
