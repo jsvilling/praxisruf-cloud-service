2c7d03fa522ccde11dd48a17d9686564e27d9916
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2021-06-22 18:10:04 +0200

    remove context path

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/PraxisCloudServiceApplication.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/PraxisCloudServiceApplication.java
@@ -3,6 +3,8 @@ package ch.fhnw.ip5.praxiscloudservice;
 import ch.fhnw.ip5.praxiscloudservice.config.FirebaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
@@ -13,4 +15,12 @@ public class PraxisCloudServiceApplication {
         SpringApplication.run(PraxisCloudServiceApplication.class, args);
     }
 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("http://localhost:5000");
+            }
+        };
+    }
 }
--- src/main/java/ch/fhnw/ip5/praxiscloudservice/web/controller/AuthController.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/web/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @AllArgsConstructor
 @Api(tags = "Authentication")
 public class AuthController {
