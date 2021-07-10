d687a7f3b907b41038fee3cd566eb5c919bb08f2
Joshua Villing <joshua.villing@mimacom.com>
2021-07-10 11:23:05 +0200

    Fix tests

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/api/dto/MinimalClientDto.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/api/dto/MinimalClientDto.java
@@ -2,6 +2,7 @@ package ch.fhnw.ip5.praxiscloudservice.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
 @Getter
 @Builder
 @AllArgsConstructor
+@EqualsAndHashCode
 public class MinimalClientDto {
     private UUID id;
     private String name;
