d9a472e85da709c1bf88d3904982764f3c5eb5d6
Joshua Villing <joshua.villing@mimacom.com>
2021-07-17 13:02:30 +0200

    Add ClientConfigurationService javadoc

--- src/main/java/ch/fhnw/ip5/praxiscloudservice/api/ClientConfigurationService.java
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/api/ClientConfigurationService.java
@@ -7,21 +7,57 @@ import java.util.Set;
 import java.util.UUID;
 
 /**
- * This interface specifies contracts retrieving the configuration of client devices.
+ * This interface specifies interactions with ClientConfigurations.
  *
  * @author J. Villing
  */
 public interface ClientConfigurationService {
 
+    /**
+     * Creates a ClientConfiguration according to the given DTO.
+     *
+     * @param configuratinoDto
+     * @throws PraxisIntercomException - If the ClientConfigurationDto is invalid
+     * @return ClientConfigurationDto
+     */
     ClientConfigurationDto createClientConfiguration(ClientConfigurationDto configuratinoDto);
 
+    /**
+     * Finds the ClientConfiguration with the given Id.
+     *
+     * @param configurationId
+     * @throws PraxisIntercomException - If no ClientConfiguration with the given id exists.
+     * @return ClientConfigurationDto
+     */
     ClientConfigurationDto findClientConfigurationById(UUID configurationId);
 
+    /**
+     * Finds all ClientConfigurations.
+     *
+     * @return Set<ClientConfigurationDto>
+     */
     Set<ClientConfigurationDto> findAllClientConfigurations();
 
+    /**
+     * Creates a ClientConfiguration according to the given DTO.
+     *
+     * @param configurationDto
+     * @throws PraxisIntercomException - If the ClientConfigurationDto is invalid.
+     * @return ClientConfigurationDto
+     */
     ClientConfigurationDto updateClientConfiguration(ClientConfigurationDto configurationDto);
 
+    /**
+     * Deletes the ClientConfiguration with the given id.
+     *
+     * @param configurationId
+     */
     void deleteClientConfigurationById(UUID configurationId);
 
-    void deleteAllClientConfigurationsById(List<UUID> filter);
+    /**
+     * Deletes ClientConfigurations with any of the given ids.
+     *
+     * @param clientConfigurationIds
+     */
+    void deleteAllClientConfigurationsById(List<UUID> clientConfigurationIds);
 }
