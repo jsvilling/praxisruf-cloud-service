90d75e96fccbed496074d1ec7abb5793c220f9b3
Joshua Villing <joshua.villing@mimacom.com>
2021-07-17 16:18:28 +0200

    Extend NotificationSendProcessServiceTest

--- src/test/java/ch/fhnw/ip5/praxiscloudservice/service/NotificationSendProcessServiceTest.java
+++ src/test/java/ch/fhnw/ip5/praxiscloudservice/service/NotificationSendProcessServiceTest.java
@@ -1,4 +1,54 @@
 package ch.fhnw.ip5.praxiscloudservice.service;
 
+import ch.fhnw.ip5.praxiscloudservice.persistence.NotificationSendProcessRepository;
+import ch.fhnw.ip5.praxiscloudservice.service.notification.NotificationSendProcessService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class NotificationSendProcessServiceTest {
+
+    @Mock
+    private NotificationSendProcessRepository notificationSendProcessRepository;
+
+    @InjectMocks
+    private NotificationSendProcessService notificationSendProcessService;
+
+    @Test
+    void createNotificationSendLogEntry() {
+        // Given
+        final UUID notificationId = UUID.randomUUID();
+        final boolean success = false;
+        final String token = "TOKEN";
+
+        // When
+        notificationSendProcessService.createNotificationSendLogEntry(notificationId, success, token);
+
+        // Then
+        verify(notificationSendProcessRepository, times(1)).saveAndFlush(any());
+    }
+
+    @Test
+    void findAllFcmTokensForFailed() {
+        // Given
+        final UUID notificationId = UUID.randomUUID();
+
+        // When
+        notificationSendProcessService.findAllFcmTokensForFailed(notificationId);
+
+        // Then
+        verify(notificationSendProcessRepository, times(1)).findAllByNotificationIdAndSuccess(eq(notificationId), eq(false));
+    }
+
+
 }
