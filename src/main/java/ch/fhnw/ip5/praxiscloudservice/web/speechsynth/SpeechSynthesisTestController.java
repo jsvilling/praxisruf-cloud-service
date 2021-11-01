80965530671b2d0123ee3815f313597d1f2f219c
Joshua Villing <joshua.villing@mimacom.com>
2021-11-01 11:15:27 +0100

    Implement aws polly poc

---
+++ src/main/java/ch/fhnw/ip5/praxiscloudservice/web/speechsynth/SpeechSynthesisTestController.java
@@ -0,0 +1,69 @@
+package ch.fhnw.ip5.praxiscloudservice.web.speechsynth;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.polly.AmazonPollyClient;
+import com.amazonaws.services.polly.model.*;
+import io.swagger.annotations.Api;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+
+import static javazoom.jl.player.FactoryRegistry.systemRegistry;
+
+@RestController
+@RequestMapping("/api/speechsynthesis")
+@Api(tags = "Speech Synthesis Test")
+public class SpeechSynthesisTestController {
+
+    private static final String SAMPLE = "Benachrichtigung empfangen";
+    private final AmazonPollyClient polly;
+    private final Voice voice;
+
+    public SpeechSynthesisTestController() {
+        polly = new AmazonPollyClient(new DefaultAWSCredentialsProviderChain(), new ClientConfiguration());
+        polly.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
+        DescribeVoicesResult describeVoicesResult = polly.describeVoices(describeVoicesRequest);
+        voice = describeVoicesResult.getVoices().stream().filter(v -> v.getLanguageName().equals("German")).findFirst().get();
+    }
+
+    @GetMapping
+    public ResponseEntity synthesizeTestAudio() {
+        final SynthesizeSpeechRequest synthReq = new SynthesizeSpeechRequest()
+                .withText(SAMPLE)
+                .withVoiceId(voice.getId())
+                .withOutputFormat(OutputFormat.Mp3);
+
+        final SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
+        final InputStream inputStream = synthRes.getAudioStream();
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        playAudio(inputStream);
+
+        return new ResponseEntity(inputStreamResource, HttpStatus.OK);
+    }
+
+    /**
+     * This is only for local testing.
+     *
+     * @param inputStream
+     * @throws JavaLayerException
+     */
+    private void playAudio(InputStream inputStream) {
+        try {
+            new AdvancedPlayer(inputStream, systemRegistry().createAudioDevice()).play();
+        } catch (Exception e) {
+            System.out.println("Could not play audio");
+        }
+    }
+
+}
