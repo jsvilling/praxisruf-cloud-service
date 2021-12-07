8ff512c4f86a0d7cc45017694dccc7eae8ed7c46
Joshua Villing <joshua.villing@mimacom.com>
2021-12-07 15:39:39 +0100

    Remove not needed methods

--- praxisruf-configuration/src/main/java/ch/fhnw/ip6/praxisruf/configuration/persistence/RegistrationRepository.java
+++ praxisruf-configuration/src/main/java/ch/fhnw/ip6/praxisruf/configuration/persistence/RegistrationRepository.java
@@ -4,17 +4,12 @@ import ch.fhnw.ip6.praxisruf.configuration.domain.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, UUID> {
 
-    List<Registration> findAll();
-
     Optional<Registration> findByClientId(UUID clientId);
-
-    Registration save(Registration registration);
 
 }
