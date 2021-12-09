3b5d2f29842da761d291c34e28b95bb77b65a24f
Joshua Villing <joshua.villing@mimacom.com>
2021-12-09 12:11:44 +0100

    Remove redundant comments

--- praxisruf-intercom/src/main/java/ch/fhnw/ip6/praxisruf/web/SocketHandler.java
+++ praxisruf-intercom/src/main/java/ch/fhnw/ip6/praxisruf/web/SocketHandler.java
@@ -12,25 +12,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-/***
- * This class handles text message inputs, websockets sessions and text message outputs
- * for binary messages extend from BinaryWebSocketHandler instead
- */
 @Component
 @Slf4j
 public class SocketHandler extends TextWebSocketHandler {
 
     final List<ClientConnection> sessions = new CopyOnWriteArrayList<>();
 
-    /***
-     * This method gets called when a message is received from a client
-     * @param session The actual websocket session that triggers this method
-     * @param message The actual recived message
-     */
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        String name = message.getPayload();
-        //Send response to all other connected sessions
         for (ClientConnection connection : sessions) {
             if (session.getId() != connection.getSession().getId()) {
                 connection.getSession().sendMessage(message);
@@ -38,26 +27,16 @@ public class SocketHandler extends TextWebSocketHandler {
         }
     }
 
-    /***
-     * This method gets called when a client establishes a connection
-     * @param session The actual websocket session that triggers this method
-     */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
-        //Add to the list of connected sessions.
         String id = session.getUri().getQuery().split("=")[1];
         sessions.add(new ClientConnection(id, session));
     }
 
-    /***
-     * This method gets called when a client closes the connection
-     * @param session The actual websocket session that triggers this method
-     */
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        //Remove to the list of connected sessions.
         sessions.removeIf(c -> c.getSession() == session);
         session.close();
     }
